fbbfbcf76c54eaa0dd3a84f4297e145f98310a8e
shaniaesguerra <shaniae47@gmail.com>
2026-09-04 11:59:29 -0600

    Fix duplicate class attributes in SkillCard component

--- components/SkillCard.tsx
+++ components/SkillCard.tsx
@@ -12,7 +12,7 @@ export default function SkillCard({ name, technologies }: SkillCardProps) {
             <h2 className="text-xl ml-4 mt-2 font-bold mb-2 text-white-800 uppercase">{name}</h2>
             <section className="flex flex-wrap gap-2 p-4">
             {technologies.map((tech, index) => (
-                    <span key={index} className="bg-white text-card text-sm font-bold font-mono px-3 py-1 rounded-full drop-shadow-[0_0_2px_var(--foreground)]  px-3 py-1 rounded-full">{tech.tech} - {tech.level}</span>
+                    <span key={index} className="bg-white text-card text-sm font-bold font-mono px-3 py-1 rounded-full drop-shadow-[0_0_2px_var(--foreground)]">{tech.tech} - {tech.level}</span>
                 ))}
             </section>
         </div>
